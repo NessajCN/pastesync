5499d776bb477b861d7067ec0e1d7d78912679fa
Nessaj <ksnessaj@hotmail.com>
2023-03-30 16:58:25 +0800

    add post handler

--- app/wx/route.ts
+++ app/wx/route.ts
@@ -1,39 +1,65 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 import { createHash } from "crypto";
 
-
-export async function GET(request: Request) {
-  // export async function GET() {
-  const { searchParams } = new URL(request.url);
-  console.log(`Request from: ${request.url}.\nParams: ${searchParams}`);
-  const signature = searchParams.get('signature');
-  const timestamp = searchParams.get('timestamp');
-  const nonce = searchParams.get('nonce');
-  const echostr = searchParams.get('echostr');
-  const reqHeaders: HeadersInit = new Headers();
-  reqHeaders.set('Content-Type', 'application/json');
-
-  if (!signature || !timestamp || !nonce || !echostr) {
-    return NextResponse.json({ success: false, message: "Invalid Request" }, { status: 400 });
-  }
+const checksig = (signature: string, timestamp: string, nonce: string) => {
   const token = process.env.TOKEN || "";
   const arr = [timestamp, nonce, token].sort();
-  const shasum = createHash('sha1');
-  arr.forEach(item => shasum.update(item));
-  const hashcode = shasum.digest('hex');
-  console.log(`hashcode: ${hashcode}`);
-  if(hashcode === signature) {
-    console.log(`echostr: ${echostr}`);
-    return new NextResponse(echostr);
+  const shasum = createHash("sha1");
+  arr.forEach((item) => shasum.update(item));
+  const hashcode = shasum.digest("hex");
+  return hashcode === signature;
+};
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  console.log(`Request from: ${req.url}.\nParams: ${searchParams}`);
+  const signature = searchParams.get("signature");
+  const timestamp = searchParams.get("timestamp");
+  const nonce = searchParams.get("nonce");
+  const echostr = searchParams.get("echostr");
+
+  if (!signature || !timestamp || !nonce || !echostr) {
+    return NextResponse.json(
+      { success: false, message: "Invalid Request" },
+      { status: 400 }
+    );
   }
 
-  // const res = await fetch(`https://data.mongodb-api.com/product/${id}`, {
-  //   headers: reqHeaders,
-  // });
-  // const product = await res.json();
+  if (checksig(signature, timestamp, nonce)) {
+    console.log(`echostr: ${echostr}`);
+    return new NextResponse(echostr);
+  } else {
+    return NextResponse.json(
+      { success: false, message: "Unauthenticated" },
+      { status: 401 }
+    );
+  }
+}
 
-  // return NextResponse.json({ product })
-  return NextResponse.json({ signature, timestamp, nonce, echostr })
-  // return NextResponse.json({hello: "world"});
+export async function POST(req: Request) {
+  const { searchParams } = new URL(req.url);
+  console.log(`Request from: ${req.url}.\nParams: ${searchParams}`);
+  const signature = searchParams.get("signature");
+  const timestamp = searchParams.get("timestamp");
+  const nonce = searchParams.get("nonce");
+  const echostr = searchParams.get("echostr");
+  // const reqHeaders: HeadersInit = new Headers();
+  // reqHeaders.set("Content-Type", "application/json");
+  if (!signature || !timestamp || !nonce || !echostr) {
+    return NextResponse.json(
+      { success: false, message: "Invalid Request" },
+      { status: 400 }
+    );
+  }
+
+  if (!checksig(signature, timestamp, nonce)) {
+    return NextResponse.json(
+      { success: false, message: "Unauthenticated" },
+      { status: 401 }
+    );
+  }
+
+  const rawbody = req.body;
+  return new NextResponse(rawbody);
 
 }
